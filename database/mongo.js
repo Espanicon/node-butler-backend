2b7c1ccc98d26b61b4b9fa7cc8be63072464dc70
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-07-17 00:34:22 -0400

    first commit

---
+++ database/mongo.js
@@ -0,0 +1,34 @@
+// database/mongo.js
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongoose = require("mongoose");
+
+let mongod = null;
+
+async function connect() {
+  if (mongod === null) {
+    // temporary database created in memory
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+
+    // connecto to database
+    const connection = await mongoose.createConnection(uri);
+    console.log("mongoose connected");
+    return connection;
+  } else {
+    console.log("Error: mongodb connection already created (mongod != null)");
+    console.log(mongod);
+  }
+}
+
+async function closeDatabase(connection) {
+  await connection.dropDatabase();
+  await connection.close();
+  await mogod.stop();
+}
+
+const db = {
+  connect,
+  closeDatabase
+};
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,66 @@
+// index.js server
+
+// imports
+//
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const path = require("path");
+const db = require("./database/mongo");
+
+morgan.token("body", (req, res) => {
+  if (req.body == null) {
+    return {};
+  } else {
+    const bodyString = JSON.stringify(req.body);
+    return bodyString.length < 120
+      ? bodyString
+      : bodyString.slice(0, 120) + "...";
+  }
+});
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(
+  morgan("> :method :url :status :body - :response-time ms \n-----------\n")
+);
+app.use(express.static(path.joing(__dirname, "www")));
+
+async function runAsync() {
+  // wait for database
+  const DB = await db.connect();
+
+  // ENDPOINTS
+  //
+  // GET proposals
+  app.get("/node-butler/cps-proposals", async (req, res) => {
+    if (req.accepts(["json", "application/json"])) {
+      // do nothing
+    } else {
+      res
+        .set("Connection", "close")
+        .status(406)
+        .json({
+          res: "Unsopported 'Content-Type'",
+          status: 406
+        });
+    }
+
+    res
+      .set("Connection", "close")
+      .status(200)
+      .json({
+        res: "Correct test query",
+        status: 200
+      });
+  });
+}
+
+runAsync();
+
+process.on("uncaughtException", err => {
+  console.log('Error: "uncaughtException"');
+  console.log(err);
+});
