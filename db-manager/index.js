7b7172c89d839f32275d2299134bec5b2815f6f5
FidelVe <43641727+FidelVe@users.noreply.github.com>
2022-07-24 02:21:04 -0400

    created recurvise tasks to check if mongodb database is online and update the database

--- db-manager/index.js
+++ db-manager/index.js
@@ -19,82 +19,130 @@ const {
 } = require("../database/services/proposal");
 const lib = require("../icon-lib/lib-no-sdk");
 
-const INTERVALS = { oneDay: 1000 * 60 * 60 * 24 };
+const INTERVALS = { oneDay: 1000 * 60 * 60 * 24, oneMinute: 1000 * 60 };
+let CONNECTION_SUCCESS = false;
+let DB_CONNECTION = null;
+let dailyInterval = null;
+
+async function connectDB() {
+  //
+  try {
+    console.log("!---------------\nChecking if mongo is online");
+    DB_CONNECTION = await DB.connect();
+
+    if (DB_CONNECTION == null) {
+      CONNECTION_SUCCESS = false;
+      console.log("mongo is offline");
+    } else {
+      CONNECTION_SUCCESS = true;
+      console.log("mongo is online");
+    }
+  } catch (err) {
+    console.log("Error trying to connect to db");
+    console.log(err);
+    CONNECTION_SUCCESS = false;
+  }
+}
 
 async function mainAsync() {
   try {
     // connecting to db
-    const db = await DB.connect();
-    const collectionId = process.env.COLLECTION_ID;
+    if (DB_CONNECTION == null) {
+      throw new Error("connection error");
+    } else {
+      console.log("Running update on database");
+      const collectionId = process.env.COLLECTION_ID;
 
-    // get the hash of each proposal in the db
-    const proposalsHash = await getProposalsHash(collectionId, db);
-    console.log(`Found ${proposalsHash.length} entries in db`);
+      // get the hash of each proposal in the db
+      const proposalsHash = await getProposalsHash(collectionId, DB_CONNECTION);
+      console.log(`Found ${proposalsHash.length} entries in db`);
 
-    // compare all the CPS proposals to the ones in the DB and get the
-    // missing ones
-    const missingProposals = await lib.cps.getCPSMissingProposals(
-      proposalsHash
-    );
-    console.log(`Found ${missingProposals.length} new proposals to add to db.`);
+      // compare all the CPS proposals to the ones in the DB and get the
+      // missing ones
+      const missingProposals = await lib.cps.getCPSMissingProposals(
+        proposalsHash
+      );
+      console.log(
+        `Found ${missingProposals.length} new proposals to add to db.`
+      );
 
-    // add each of the missing proposals to the db
-    if (missingProposals.length > 0) {
-      console.log("Updating db");
-      for (let eachProposal of missingProposals) {
-        console.log(
-          `Adding proposal with hash: ${eachProposal.proposal["ipfs_hash"]}`
-        );
-        const newAddedProposal = await createProposal(
-          eachProposal.proposal,
-          collectionId,
-          db
-        );
-        console.log(`Result: ${newAddedProposal.status}`);
-
-        // if proposal added successfully update comments
-        if (newAddedProposal.status === "SUCCESS") {
-          console.log(`Adding comments to proposal entry in db`);
-          const updatedProposal = await updateProposalCommentsByProposalId(
-            eachProposal.comments.data,
-            newAddedProposal.message["_id"],
-            collectionId,
-            db
+      // add each of the missing proposals to the db
+      if (missingProposals.length > 0) {
+        console.log("Updating db");
+        for (let eachProposal of missingProposals) {
+          console.log(
+            `Adding proposal with hash: ${eachProposal.proposal["ipfs_hash"]}`
           );
+          const newAddedProposal = await createProposal(
+            eachProposal.proposal,
+            collectionId,
+            DB_CONNECTION
+          );
+          console.log(`Result: ${newAddedProposal.status}`);
+
+          // if proposal added successfully update comments
+          if (newAddedProposal.status === "SUCCESS") {
+            console.log(`Adding comments to proposal entry in db`);
+            const updatedProposal = await updateProposalCommentsByProposalId(
+              eachProposal.comments.data,
+              newAddedProposal.message["_id"],
+              collectionId,
+              DB_CONNECTION
+            );
+          }
         }
+
+        console.log("Finish updating db");
+      } else {
+        console.log("No update done on local db");
       }
 
-      console.log("Finish updating db");
-    } else {
-      console.log("No update done on local db");
+      // FOR TESTING DELETES ALL ITEMS IN DB
+      // for (let eachHash of proposalsHash) {
+      //   console.log(`deleting: ${eachHash}`);
+      //   await deleteOneProposalByProposalHash(eachHash, collectionId, DB_CONNECTION);
+      // }
+      // FOR TESTING
+
+      // closing connection to db
+      await DB.closeDatabase(DB_CONNECTION);
+      console.log("db closed");
     }
-
-    // FOR TESTING DELETES ALL ITEMS IN DB
-    // for (let eachHash of proposalsHash) {
-    //   console.log(`deleting: ${eachHash}`);
-    //   await deleteOneProposalByProposalHash(eachHash, collectionId, db);
-    // }
-    // FOR TESTING
-
-    // closing connection to db
-    await DB.closeDatabase(db);
-    console.log("db closed");
   } catch (err) {
     console.log("Error running main async code");
     console.log(err);
   }
 }
 
-mainAsync();
+// set recursive tasks
+//
+// set task that runs every minute to check if mongo is online
+const task1 = setInterval(async () => {
+  await connectDB();
 
-// set test of updating db to run once every day
-const task = setInterval(() => {
-  mainAsync();
+  // if mongo is online run one check inmediatly
+  if (CONNECTION_SUCCESS) {
+    await mainAsync();
+  }
+}, INTERVALS.oneMinute);
+
+// set task that runs day to update db IF mongo is online
+const task2 = setInterval(async () => {
+  if (CONNECTION_SUCCESS) {
+    await mainAsync();
+  } else {
+    console.log("mongo is offline, skipping check");
+  }
 }, INTERVALS.oneDay);
 
 // Enable graceful stop
 process.once("SIGINT", () => {
   console.log("Terminating execution");
-  clearInterval(task);
+  clearInterval(task1);
+  clearInterval(task2);
+  console.log("recursive tasks cleared");
 });
-process.once("SIGTERM", () => clearInterval(task));
+process.once("SIGTERM", () => {
+  clearInterval(task1);
+  clearInterval(task2);
+});
